3b2469cae67812b2b392f5cd7dfe60a37261e8c2
markus8006 <guilhermemarkus8@gmail.com>
2025-09-20 00:32:43 +0000

    Tipagem em adapters

--- src/adapters/base_adapter.py
+++ src/adapters/base_adapter.py
@@ -1,24 +1,27 @@
 from abc import ABC, abstractmethod
+from typing import Dict, Any, Optional, List
 
 class BaseAdapter(ABC):
     """Interface para todos os protocolos de comunicação com CLPs"""
 
     @abstractmethod
-    def connect(self, address: str, **kwargs):
+    def connect(self, clp: Dict[str, Any], port : int|None) -> bool:
         """Estabelece conexão com o CLP"""
         pass
 
     @abstractmethod
-    def read_tag(self, tag: str):
+    def disconnect(self, clp: Dict[str, Any]):
+        """Fecha a conexão"""
+        pass
+
+
+    @abstractmethod
+    def read_tag(self, clp: Dict[str, Any], address: int, count: int = 1) -> Optional[List[int]]:
         """Lê uma tag/registrador"""
         pass
 
     @abstractmethod
-    def write_tag(self, tag: str, value):
+    def write_tag(self, clp: Dict[str, Any], address: int, value:Any) -> bool:
         """Escreve em uma tag/registrador"""
         pass
 
-    @abstractmethod
-    def disconnect(self):
-        """Fecha a conexão"""
-        pass
